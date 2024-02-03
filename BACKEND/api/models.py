1dbeaaac38d0e0f8b7b0f59bdbc59db95fa7c6c2
Larry La <larryquocla@gmail.com>
2024-02-03 00:16:56 -0800

    backend finally is connected finally omg its actually connected lets goo

--- BACKEND/api/models.py
+++ BACKEND/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Restroom(models.Model):
     location = models.CharField(unique=True, max_length=100)
     upvotes = models.IntegerField(default=0)
+    image = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.location
