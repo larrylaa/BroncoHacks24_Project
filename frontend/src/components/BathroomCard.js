4e43e265fb9389931635d105af242c03fa98b888
Michael <mwu250@gmail.com>
2024-02-02 17:37:35 -0800

    basic ranking component

---
+++ frontend/src/components/BathroomCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/BathroomCard.css";
+
+import TempCard from "../images/lib 2nd floor.jpg";
+import Counter from "./Counter";
+
+const BathroomCard = (props) => {
+  return (
+    <div className="card">
+
+      <div className="image-container">
+        <img src={TempCard} alt="temp img" />
+      </div>
+
+      <div className="title">
+        <h2>{props.title}</h2>
+      </div>
+      
+      <div className="counter-container">
+        <Counter temp="vote" />
+        {/* <h2>temp</h2> */}
+      </div>
+
+    </div>
+  );
+};
+
+export default BathroomCard;
--- frontend/src/screens/Ranking.js
+++ frontend/src/screens/Ranking.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import '../styles/Ranking.css';
+
+import BathroomCard from '../components/BathroomCard';
+
 const Ranking = () => { 
   return (
     <div>
-      <h2>RANKING</h2>
+      <h1>RANKING</h1>
+      <BathroomCard title="LOCATION/NAME"/>
     </div>
   );
 };
