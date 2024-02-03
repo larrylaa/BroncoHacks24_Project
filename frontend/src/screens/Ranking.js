067f60e3dee5d81ebe321cd35ed0343f2352ca3f
Michael <mwu250@gmail.com>
2024-02-02 19:02:08 -0800

    help with fetch data

--- frontend/src/screens/Ranking.js
+++ frontend/src/screens/Ranking.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import '../styles/Ranking.css';
 
@@ -8,6 +8,15 @@ const Ranking = () => {
 
   const bathrooms = [{location: "location 1", upvotes: 8}, {location: "location 2", upvotes: 2}]
   
+  // const [bathrooms, setBathrooms] = useState([]);
+
+  // useEffect(() => {
+    // Fetch data from the API
+  //   fetch('http://localhost:3000/restroom/')
+  //     .then(response => response.json())
+  //     .then(data => setBathrooms(data))
+  //     .catch(error => console.error('Error fetching data:', error));
+  // }, []);
 
   return (
     <div>
