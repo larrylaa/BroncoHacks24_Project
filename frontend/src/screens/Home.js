7f51d6b0a7658b562e7ce55411ed0b4d27e0b519
Michael <mwu250@gmail.com>
2024-02-02 23:16:03 -0800

    Good enough for now ig

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,17 +1,41 @@
 import React from "react";
 import "../styles/Home.css";
-
-import temp from "../images/lib 2nd floor.jpg";
-
-// import BathroomMap from '../images/cppmap.jpg';
-
-import '../styles/Home.css';
+import BathroomMap from "../images/cppmap.jpg";
+import BillyBronco from "../images/superhero-billy1.png";
+import CPPLogo from "../images/Cal_Poly_Pomona_Broncos_logo.png";
 
 const Home = () => {
   return (
+    // <div className="home">
+    //   <h1 className="home-title">Rate My Restroom</h1>
+    //   <div className="image-container">
+    //     <div>temp</div>
+    //     {/* <img src={BathroomMap} alt="temp img" /> */}
+    //   </div>
+
+    // </div>
+
     <div className="home">
-      <h1 className="home-title">HOMESCREEN</h1>
-      <div className="image-container">MIDDLE</div>
+      <div className="home-title">Rate My Restroom</div>
+      <img className="map" src={BathroomMap} />
+      <div className="bottom-container">
+        <img className="cpp-logo" src={CPPLogo}/>
+        <div className="paragraph">
+          This website serves as a valuable tool for the Cal Poly Pomona
+          community, aiding in the identification and assessment of restroom
+          facilities across the campus. By leveraging data analytics, it
+          meticulously tracks and analyzes restroom usage patterns, providing
+          insights into which facilities experience the highest foot traffic and
+          which maintain optimal cleanliness standards. Through its
+          comprehensive reporting system, users can easily identify trends, make
+          informed decisions regarding restroom utilization, and contribute to
+          the overall campus maintenance efforts. Additionally, this platform
+          fosters a culture of accountability and stewardship, encouraging
+          individuals to uphold restroom hygiene standards and ensuring that
+          facilities remain comfortable and sanitary for all campus occupants.
+        </div>
+        <img className="billy-bronco" src={BillyBronco}/>
+      </div>
     </div>
   );
 };
