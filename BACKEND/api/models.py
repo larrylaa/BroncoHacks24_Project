b4c7b2c96209cf3843c8bfc431d03675c17e3b62
Larry La <larryquocla@gmail.com>
2024-02-02 17:04:01 -0800

    feat: restroom db model

--- BACKEND/api/models.py
+++ BACKEND/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Restroom(models.Model):
+    location = models.CharField(unique=True, max_length=100)
+    upvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.location
+
