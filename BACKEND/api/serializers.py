5ec8bc5a1cf0856d19b772dd9cb078cf3408583a
Larry La <larryquocla@gmail.com>
2024-02-02 17:30:22 -0800

    feat: restroom route

---
+++ BACKEND/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from.models import *
+
+class RestroomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restroom
+        fields = ('location','upvotes')
+
+
+    location = models.CharField(unique=True, max_length=100)
+    upvotes = models.IntegerField(default=0)
